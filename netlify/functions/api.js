a27715634667f7569b39f0490f2a62c94f8871be
shital unde <shital@rsmart.fi>
2025-03-26 14:40:33 +0530

    010

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -3,19 +3,19 @@ const serverless = require('serverless-http');
 
 const app = express();
 
-// No body-parser needed - express includes it now
+const router = express.Router();
 app.use(express.json());
 
 // Your API routes
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.json({ message: 'API is working!' });
 });
 
-app.get('/api/ok', (req, res) => {
+router.get('/ok', (req, res) => {
     res.json({ message: 'API1 is working!' });
 });
 
-app.post('/api/ok2', (req, res) => {
+router.post('/api/ok2', (req, res) => {
     res.json({ message: 'API2 is working!' });
 });
 
